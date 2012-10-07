075332ce19f2fcc1e5514e6e1487a0e7d6c80b23
Michal Staniaszek <m.staniaszek@gmail.com>
2012-10-07 22:17:44 +0100

    reading parameters into arrays

--- code/prototypes/file_util.c
+++ code/prototypes/file_util.c
@@ -2,17 +2,15 @@
 #include <time.h>
 
 #define MAX_DATE_LENGTH 26
+#define MAX_PARAM_STRING_LENGTH 100
 
-/* int main(int argc, char *argv[]) */
-/* { */
-    
-/*     double a[3] = {1.0,2.0,3.0}; */
-/*     double b[3] = {2,3,4}; */
-    
-/*     double_to_file("asdf", a, b, 3); */
-    
-/*     return 0; */
-/* } */
+int main(int argc, char *argv[])
+{
+
+    get_parameters("params.txt");
+        
+    return 0;
+}
 
 
 char* generate_outfile()
@@ -29,6 +27,73 @@ char* generate_outfile()
     
     return fname;
         
+}
+
+/* 
+ * Gets parameters from a file. The first line of the parameter file should be an
+ * integer representing the number of parameters to be read in.
+ */
+void get_parameters(char* filename)
+{
+    char *line = malloc(MAX_PARAM_STRING_LENGTH * sizeof(char));
+    FILE *fp = fopen(filename, "r");
+    int pnamelen, pvallen, i, arrsize;
+
+    line = fgets(line, 10, fp);
+    arrsize = atoi(line);
+
+    if (arrsize == 0){
+	printf("Number of parameters in parameter file not provided. The first line of the file should be a single integer representing the number of parameters in the file.");
+	exit(1);
+    }
+    
+    char **params = malloc(arrsize * sizeof(char*));
+    char **values = malloc(arrsize * sizeof(char*));
+        
+    for (i = 0; (line = fgets(line, MAX_PARAM_STRING_LENGTH, fp)) != NULL; ++i){
+	// Attempt at dynamic allocation of parameter lists. Try again later?
+	/* if (i >= arrsize){ */
+	/*     char** tmp = malloc(arrsize * sizeof(char)); */
+	/*     char** tmp2 = malloc(arrsize * sizeof(char)); */
+	    	    
+	/*     for (j = 0; j < arrsize; ++j){ */
+	/* 	tmp[j] = malloc(strlen(params[j])); */
+	/* 	//tmp2[j] = malloc(strlen(values[j])); */
+
+	/* 	tmp[j] = params[j]; */
+	/* 	//tmp2[j] = values[j]; */
+	/*     } */
+	    
+	/*     params = realloc(params, arrsize * 2); */
+	/*     values = realloc(values, arrsize * 2); */
+	
+	/*     for (j = 0; j < arrsize; ++j){ */
+	/* 	params[j] = tmp[j]; */
+	/* 	//values[j] = tmp2[j]; */
+	/*     } */
+	/*     arrsize *= 2; */
+	/*     params = tmp; */
+	/*     values = tmp2; */
+	    
+	/*     free(tmp); */
+	/*     free(tmp2); */
+	/* } */
+	pnamelen = strchr(line, ' ') - line;
+	params[i] = malloc(pnamelen * sizeof(char));
+	pvallen = strchr(line, '\0') - strchr(line, ' ');
+	values[i] = malloc(pvallen * sizeof(char));
+	
+	strncpy(params[i], line, pnamelen);
+	strncpy(values[i], line + pnamelen + 1, pvallen);
+	
+	printf("%s\n", params[i]);
+	printf("%s\n", values[i]);
+    }
+
+    free(params);
+    free(values);
+    free(line);
+        	
 }
 
 void double_to_file(char* filename, char* mode, double* arr1, double* arr2, int len)
