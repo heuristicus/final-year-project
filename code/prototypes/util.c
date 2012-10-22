857525760e22b653dcab7b80d922f621b412dc46
Michal Staniaszek <m.staniaszek@gmail.com>
2012-10-22 21:07:55 +0100

    File for utility functions

---
+++ code/prototypes/util.c
@@ -0,0 +1,12 @@
+#include "poisson.h"
+#include "estimator.h"
+
+void free_pointer_arr(void **arr, int length)
+{
+    int i;
+    
+    for (i = 0; i < length; ++i){
+	free(arr[i]);
+    }
+    free(arr);
+}
