2d450804e6b0844f83203fcb9ef16df0831fdd30
Michal Staniaszek <m.staniaszek@gmail.com>
2012-10-23 23:59:15 +0100

    Compiles correctly, but errors on loading libraries.
    error while loading shared libraries: libfile_util.so: cannot open shared object file: No such file or directory

--- code/prototypes/estimator.h
+++ code/prototypes/estimator.h
@@ -1,17 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#ifndef _ESTIMATOR_H
+#define _ESTIMATOR_H
+
 // estimatorIWLS.c
 double** get_subintervals(double time, int subintervals);
 void free_pointer_arr(void **arr, int length);
 double get_interval_midpoint(int interval_number, double time, int subintervals);
 double* initialise_weights(int subintervals);
 
-// math_util.c
-long double fact(int i);
-double prob_num_events_in_time_span(double t_start, double t_end, double lambda, int k);
-int rolling_window(double *event_times, int num_events, double start_time, double timespan, int *output_array);
-
-// util.c
-void free_pointer_arr(void **arr, int length);
-
+#endif
---
+++ code/prototypes/general_util.h
@@ -0,0 +1,6 @@
+#ifndef _GEN_UTIL_H
+#define _GEN_UTIL_H
+
+void free_pointer_arr(void **arr, int length);
+
+#endif
