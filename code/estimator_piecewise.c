95325888e9f136928f2348e0a2502a091b93f5d5
Michal Staniaszek <m.staniaszek@gmail.com>
2012-11-28 20:42:13 +0000

    Memory leaks fixed - do this when writing, rather than afterwards.

--- code/estimator_piecewise.c
+++ code/estimator_piecewise.c
@@ -28,7 +28,9 @@ int main(int argc, char *argv[])
     double **piece = piecewise_estimate(argv[1], argv[2], 0, 100, 5, 3, 10, 15);
     
     output_estimates("testout.txt", piece, 5);
-            
+
+    free_pointer_arr((void**)piece, 5);
+                
     return 0;
 }
 
@@ -43,7 +45,7 @@ double** piecewise_estimate(char *event_file, char *output_file, double interval
     
     int i = 0;
     double default_interval_length;
-    if (max_breakpoints == 0)
+    if (max_breakpoints == 0) // if there are no breakpoints, we work on the whole interval
 	default_interval_length = interval_end - interval_start;
     else 
 	default_interval_length = (interval_end - interval_start) / max_breakpoints;
@@ -60,7 +62,7 @@ double** piecewise_estimate(char *event_file, char *output_file, double interval
 	
 	// Don't bother extending the line if we are at the end of the overall interval,
 	// or if we are estimating the last line segment.
-	if (end_time < interval_end || i == max_breakpoints - 1){
+	if (end_time < interval_end || i < max_breakpoints - 1){
 	    int tmp;
 	    
 	    if ((tmp = extend_estimate(event_file, interval_estimate, end_time, max_extension, 10)) != -1){
@@ -82,6 +84,7 @@ double** piecewise_estimate(char *event_file, char *output_file, double interval
 	++i;
 	start_time = end_time; // The start of the next interval is the end of the current
 	end_time = start_time + default_interval_length > interval_end ? interval_end : start_time + default_interval_length;
+	free(interval_estimate);
     } while(i < max_breakpoints && end_time <= interval_end);
 
     return interval_data;
@@ -96,27 +99,34 @@ double extend_estimate(char *event_file, double *interval_estimate, double start
 		       double max_extension, int num_subintervals)
 {
     int i;
-    
+    double retval = -1;
+        
     for (i = 1; i < 5; ++i) {
 
 	double end_time = start_time + max_extension/i; // probably need something better than this
     
 	// Don't really need to do this every time - just truncate the array
 	double *events = get_event_data_interval(start_time, end_time, event_file);
-	events++; // Increment the pointer past the first array location which contains the size
-	
-	int *bin_counts = sum_events_in_interval(events, events[-1] -1 , start_time, end_time, num_subintervals);
+	double *tmp = events; // keep the original pointer so we can mess around with the other
+	int event_num = events[0] - 1;
+	events++;
+		
+	int *bin_counts = sum_events_in_interval(events, event_num, start_time, end_time, num_subintervals);
 	double *midpoints = get_interval_midpoints(start_time, end_time, num_subintervals);
     
 	int res = pmf_check(midpoints, bin_counts, interval_estimate[0], interval_estimate[1], num_subintervals);
     
-	if (res)
-	    return end_time;
-	else
-	    continue;
+	free(tmp);
+	free(bin_counts);
+	free(midpoints);
+
+	if (res){
+	    retval = end_time;
+	    break;
+	}
     }
     
-    return -1;
+    return retval;
 }
 
 /*
@@ -134,6 +144,8 @@ int pmf_check(double *midpoints, int *bindata, double a, double b, int num_subin
     printf("Mean probability: %lf\n", sum/num_subintervals);
 
     int ret = pmf_threshold_check(pmfs, num_subintervals);
+
+    free(pmfs);
     
     return ret;
 }
