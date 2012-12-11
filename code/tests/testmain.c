50c2c00a81e7a5552ae961fbf1bb78b0f7cfc7ac
Michal Staniaszek <m.staniaszek@gmail.com>
2012-12-11 16:28:36 +0000

    top level test file

---
+++ code/tests/testmain.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <check.h>
+#include <stdio.h>
+#include "tests.h"
+
+int main(int argc, char *argv[])
+{
+    int number_failed;
+
+    SRunner *sr = srunner_create(math_util_suite());
+    srunner_add_suite(sr, general_util_suite());
+    srunner_run_all(sr, CK_VERBOSE);
+    number_failed = srunner_ntests_failed(sr);
+    srunner_free(sr);
+    return number_failed == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
+    
+    return 0;
+}
